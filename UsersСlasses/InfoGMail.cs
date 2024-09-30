dbf0b94c1fecfda1e55f05f48bfc80ac5cf01150
fedotovaaaaa <156669756+fedotovaaaaa@users.noreply.github.com>
2024-09-30 14:40:22 +0300

    Initial commit

---
+++ UsersСlasses/InfoGMail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.UsersClasses;
+
+namespace WindowsFormsApp1.UsersClasses
+{
+    internal class InfoGMail : InfoEmail
+    {
+        public InfoGMail(StringPair emailAdressTo, string subject, string body) :
+           base(emailAdressTo, subject, body)
+        {
+            SmtpClientAdress = "smtp.gmail.com";
+            EmailAdressFrom = new StringPair("svecanastasia39@gmail.com", "Федотова Анастасия Яновна");
+            EmailPassword = "dgtu kybk ntam jzvq";
+            Port = 587;
+        }
+    }
+}
